7881ccef8914b3776f5c956208d63943fa0b30ca
Avijit Das <140038956+avijitdas126@users.noreply.github.com>
2024-09-15 14:27:10 +0530

    Fixing some error

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ bot.hears('/now', async (ctx) => {
   let weather=data.weather[0].description
 await ctx.deleteMessage(msgid)
     try{
-await bot.telegram.sendPhoto( bot.chat.id,{url:imgurl},{caption:`Name: ${data.name}\nTemperature: ${temp} degree Celsius\nWind: ${wind} kph\nWeather: ${weather}\nHumidity: ${data.main.humidity} percentenge\nNation: ${data.sys.country}`} );
+await bot.telegram.sendPhoto( ctx.chat.id,{url:imgurl},{caption:`Name: ${data.name}\nTemperature: ${temp} degree Celsius\nWind: ${wind} kph\nWeather: ${weather}\nHumidity: ${data.main.humidity} percentenge\nNation: ${data.sys.country}`} );
     }
     catch(err){
       ctx.reply('We are facing some difficulties')
@@ -102,7 +102,7 @@ bot.on(message('text'), async (ctx) => {
   let weather=data.weather[0].description
 await ctx.deleteMessage(msgid)
     try{
-await bot.telegram.sendPhoto( bot.chat.id,{url:imgurl},{caption:`Name: ${data.name}\nTemperature: ${temp} degree Celsius\nWind: ${wind} kph\nWeather: ${weather}\nHumidity: ${data.main.humidity} percentenge\nNation: ${data.sys.country}`} );
+await bot.telegram.sendPhoto( ctx.chat.id,{url:imgurl},{caption:`Name: ${data.name}\nTemperature: ${temp} degree Celsius\nWind: ${wind} kph\nWeather: ${weather}\nHumidity: ${data.main.humidity} percentenge\nNation: ${data.sys.country}`} );
     }
     catch(err){
       ctx.reply('We are facing some difficulties')
