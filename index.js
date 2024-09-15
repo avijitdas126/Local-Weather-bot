259fe51cc0587f783c333cb886d187d8996b5582
Avijit Das <140038956+avijitdas126@users.noreply.github.com>
2024-09-15 17:25:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ async function count(id) {
     const user = await Telbot.findOne({ userid: id });
 
     if (user) {
-      // If user exists, increment the count
+      // Increment the count field safely
       const newCount = (user.count || 0) + 1;
 
-      // Update the count field
+      // Update the count field in the database
       await Telbot.updateOne(
         { userid: id },
         { $set: { count: newCount } }
       );
 
-      console.log('Count updated successfully:');
+      console.log('Count updated successfully:', newCount);
     } else {
       console.log('User not found');
     }
   } catch (error) {
-    console.log('Error occurred:', error.message);
+    console.log('Error occurred:', error);
   }
 }
 
@@ -61,18 +61,26 @@ bot.start(async (ctx) => {
 });
 bot.hears('/now', async (ctx) => {
     let city=await Tel.find({userid:ctx.update.message.from.id})
-    if(city.length){
+    count(ctx.update.message.from.id)
+      if(city){
+        let { message_id:msgid }=await ctx.replyWithSticker('CAACAgIAAxkBAAIBmmbmtyxl__PM1i4wsKcHKljraZGsAAIwFAACV03ASHMUDFXjRXH1NgQ')
       city=city[0].local.toLowerCase()
       let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${token[random()]}&units=metric`)
      data=await data.json()
      if(data.cod==200){
-     let temp=data.main.temp;
-     let wind=data.wind.speed
-     let weather=data.weather[0].description
-   
-   ctx.reply( `Name: ${data.name}\nTemperature: ${temp} degree Celsius\nWind: ${wind} kph\nWeather: ${weather}\nHumidity: ${data.main.humidity} percentenge\nNation: ${data.sys.country}` );
-   
-     }
+    let imgurl=`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+  let temp=data.main.temp;
+  let wind=data.wind.speed
+  let weather=data.weather[0].description
+await ctx.deleteMessage(msgid)
+    try{
+await bot.telegram.sendPhoto( ctx.message.from.id,{url:imgurl},{caption:`Name: ${data.name}\nTemperature: ${temp} degree Celsius\nWind: ${wind} kph\nWeather: ${weather}\nHumidity: ${data.main.humidity} percentenge\nNation: ${data.sys.country}`} );
+    }
+    catch(err){
+      ctx.reply('We are facing some difficulties')
+      console.log(err);
+    }
+    }
      else{
        await ctx.deleteMessage(msgid)
        ctx.reply(`${data.message}. Set Your local city correctly using /setlocal [Name of your local city]`)
@@ -84,11 +92,17 @@ bot.hears('/now', async (ctx) => {
 
 
 })
+
+bot.help((ctx)=>{
+  count(ctx.update.message.from.id)
+  ctx.reply('Bot Command:\n\n1.\t /now -\t\t Know your local weather immediately.\n2. /weather [Name of another city] -\t Know the weather of another city immediately.\n3. /setlocal [Name of your local city] -\t Set your local city.\n Further any problem contact our developer @avijit126');
+})
 bot.on(message('text'), async (ctx) => {
     let text=(ctx.update.message.text)
     if(text.includes('/setlocal')){
       count(ctx.update.message.from.id)
         let city=text.split(' ')[1]
+         if(city){
         let id=ctx.update.message.from.id
          let obj={useid:id,local:city}
        try {
@@ -101,26 +115,48 @@ bot.on(message('text'), async (ctx) => {
         ctx.reply('Your Local City is added sucessfully.\nIf immediately know your current weather when you\nwill type /now. 😊')
       } catch (err) {
         ctx.reply('Your Local City is added unsucessfully.Try again.')
-        console.error('Error inserting/updating user:', err);
+        console.log('Error inserting/updating user:', err.message);
+      }
+         }
+      else{
+        ctx.reply('Enter a valid city using  /setlocal [Name of another city] command\nExample:- /setlocal Kolkata,/setlocal Tokyo');
       }
     }
     else if(text.includes('/weather')){
-        let city=text.split(' ')[1]
-        let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OpenWeather_Token}&units=metric`)
+      count(ctx.update.message.from.id)
+  let { message_id:msgid }=await ctx.replyWithSticker('CAACAgIAAxkBAAIBmmbmtyxl__PM1i4wsKcHKljraZGsAAIwFAACV03ASHMUDFXjRXH1NgQ')
+  let city=text.split(' ')[1]
+      if(city){
+  let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${token[random()]}&units=metric`)
   data=await data.json()
   if(data.cod==200){
     let imgurl=`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
   let temp=data.main.temp;
   let wind=data.wind.speed
   let weather=data.weather[0].description
-
-ctx.reply( `Name: ${data.name}\nTemperature: ${temp} degree Celsius\nWind: ${wind} kph\nWeather: ${weather}\nHumidity: ${data.main.humidity} percentenge\nNation: ${data.sys.country}` );
-
-  }
+await ctx.deleteMessage(msgid)
+    try{
+await bot.telegram.sendPhoto( ctx.message.from.id,{url:imgurl},{caption:`Name: ${data.name}\nTemperature: ${temp} degree Celsius\nWind: ${wind} kph\nWeather: ${weather}\nHumidity: ${data.main.humidity} percentenge\nNation: ${data.sys.country}`} );
+    }
+    catch(err){
+      ctx.reply('We are facing some difficulties')
+      console.log(err);
+    }
+    }
   else{
+    await ctx.deleteMessage(msgid)
     ctx.reply(`${data.message}. Set Your local city correctly using  /weather [Name of another city]`)
   }
     }
+      else{
+        await ctx.deleteMessage(msgid)
+        ctx.reply('Enter a valid city using  /weather [Name of another city] command\nExample:- /weather Kolkata,/weather Tokyo');
+      }
+    }
+  else{
+    count(ctx.update.message.from.id)
+    ctx.reply(`This bot only responsive on below this commands\n1.\t /now -\t\t Know your local weather immediately.\n2. /weather [Name of another city] -\t Know the weather of another city immediately.\n3. /setlocal [Name of your local city] -\t Set your local city.`)
+  }
 })
 //launch a bot
 bot.launch();
